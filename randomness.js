2e2919f3d7271bb9d8705bfccdf200ad10da0333
arASC6 <hiers256@gmail.com>
2019-07-11 11:41:51 -0400

    second commit

---
+++ randomness.js
@@ -0,0 +1,34 @@
+/ GLOBAL VARIABLES //
+/////////////////////
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+const foods = [
+"pizza",
+"burgers",
+"rice and beans",
+"lasagna",
+"macaroni and cheese",
+"baked potatoes"
+];
+const students = [
+"Bob",
+"Martin",
+"Danny",
+"Madhi",
+"Harry",
+"Sean",
+"Robert"
+];
+
+//////////////////////////////////
+// END OF GLOBAL VARIABLES //
+/////////////////////////////////
+
+randomNumber
+
+randomLetter
+
+randomFood
+
+randomStudent
+
+randomGroup
